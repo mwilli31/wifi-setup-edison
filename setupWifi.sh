4a85763d5cdf3c6110127f2c364e1974562cd714
Ciancimino <212570176@logon.ds.ge.com>
2016-07-15 15:40:56 -0400

    Wifi script updated

--- setupWifi.sh
+++ setupWifi.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#!/bin/bash
 
 #   wifi-setup-edison
 #
@@ -30,14 +30,14 @@ do
                 -s)
                         shift
                         NET_SSID="$1"
-			                  NET_SSID_ARG="true"
-			                  shift
+			NET_SSID_ARG="true"
+			shift
                         ;;
                 -p)
                         shift
                         NET_PASS="$1"
-			                  NET_PASS_ARG="true"
-			                  shift
+			NET_PASS_ARG="true"
+			shift
                         ;;
                 -e)
                         shift
@@ -61,26 +61,26 @@ if [ "$NET_PASS_ARG" == "false" ] || [ "$NET_PASS" == "" ]; then
         echo "Network Password argument -p required"
 	exit 1
 fi
-if [ "$NET_ENCR_ARG" == "false" ] || [ "$NET_ENCR" == "" ]; then
-        echo "Network Encryption Type argument -e required"
-	exit 1
-fi
+#if [ "$NET_ENCR_ARG" == "false" ] || [ "$NET_ENCR" == "" ]; then
+#       echo "Network Encryption Type argument -e required"
+#	exit 1
+#fi
 
 ###############################################################
 #################### PLEASE EDIT THIS PART ####################
 ###############################################################
 
 # SSID (aka. network name).
-SSID='$NET_SSID'
+#SSID='$NET_SSID'
 
 # Network encryption method.
 # * 'WPA' for WPA-PSK/WPA2-PSK (note: most Wi-Fi networks use WPA);
 # * 'WEP' for WEP;
 # * 'Open' for open network (aka. no password).
-ENCRYPTION='$NET_ENCR'
+#ENCRYPTION='$NET_ENCR'
 
 # Network password. (WPA-PSK/WPA2-PSK password, or WEP key)
-PASSWORD='$NET_PASS'
+#PASSWORD='$NET_PASS'
 
 ###############################################################
 ####################   OK. STOP EDITING!   ####################
@@ -91,21 +91,13 @@ if [ $(id -u) -ne 0 ]; then
   exit 1
 fi
 
-NETID=$(wpa_cli add_network | tail -n 1)
-wpa_cli set_network $NETID ssid \"$SSID\"
-case $ENCRYPTION in
-'WPA')
-    wpa_cli set_network $NETID key_mgmt WPA-PSK
-    wpa_cli set_network $NETID psk \"$PASSWORD\"
-    ;;
-'WEP')
-    wpa_cli set_network $NETID wep_key0 $PASSWORD
-    wpa_cli set_network $NETID wep_key1 $PASSWORD
-    wpa_cli set_network $NETID wep_key2 $PASSWORD
-    wpa_cli set_network $NETID wep_key3 $PASSWORD
-    ;;
-*)
-    ;;
-esac
-wpa_cli enable_network $NETID
-wpa_cli save_config
+cat wifi1.txt > /etc/network/interfaces
+echo "    wpa-ssid $NET_SSID" >> /etc/network/interfaces
+echo "    wpa-psk $NET_PASS" >> /etc/network/interfaces
+cat wifi2.txt >> /etc/network/interfaces
+
+echo "Restarting internet with new settings"
+ifdown wlan0
+ifup wlan0
+
+echo "Setup Complete"
